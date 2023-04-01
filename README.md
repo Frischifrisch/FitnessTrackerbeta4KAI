b7a9f39ef2aa237e28efdd3e1a86c466f0ec9fb8
Christian Frischholz <christian_frischholz@web.de>
2023-04-01 10:42:42 +0200

    Der Commitvorgang wurde aufgrund einer leeren Commitnachricht abgebrochen.

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
  FitneßTrackerβeta4KAI
 
 Ich versuche nur irgendwie n bisschen Android Studio zu lernen und. Ja. "Hello World."
+
+Übungsauswahl: Die App sollte eine Liste von Übungen anzeigen, aus der der Benutzer auswählen kann. Der Benutzer kann dann auswählen, welche Übungen er während seiner Trainingseinheit durchführen möchte.
+
+Übungsverfolgung: Die App sollte die Anzahl der Wiederholungen, die Dauer der Übung und andere relevante Daten während des Trainings aufzeichnen. Dies ermöglicht es dem Benutzer, seine Fortschritte zu verfolgen.
+
+Trainingsplanung: Die App sollte es dem Benutzer ermöglichen, einen Trainingsplan zu erstellen, um sicherzustellen, dass er seine Trainingsziele erreicht. Der Benutzer kann dann seinen Plan einhalten und seine Fortschritte verfolgen.
+
+Benutzerprofile: Die App sollte Benutzerprofile unterstützen, damit Benutzer ihre persönlichen Daten und Ziele eintragen können. Dies ermöglicht es der App, personalisierte Trainingspläne und Empfehlungen zu erstellen.
+
+Statistiken: Die App sollte dem Benutzer detaillierte Statistiken zu seinen Trainingsfortschritten anzeigen. Dies kann dem Benutzer helfen, seine Fortschritte zu verfolgen und seine Trainingsroutine anzupassen, um seine Ziele zu erreichen.
--- app/src/androidTest/java/DatabaseHelper.java
+++ app/src/androidTest/java/DatabaseHelper.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class DatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    // Name und Version der Datenbank
+    private static final String DATABASE_NAME = "fitness.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // Konstruktor der Klasse
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // Methode zum Erstellen der Tabellen der Datenbank
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Tabelle für die Übungen erstellen
+        db.execSQL("CREATE TABLE exercises (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, sets INTEGER, reps INTEGER, weight REAL, time INTEGER)");
+
+        // Tabelle für die Workouts erstellen
+        db.execSQL("CREATE TABLE workouts (_id INTEGER PRIMARY KEY AUTOINCREMENT, date DATE)");
+
+        // Tabelle für die Übungen in den Workouts erstellen
+        db.execSQL("CREATE TABLE workout_exercises (workout_id INTEGER, exercise_id INTEGER, FOREIGN KEY(workout_id) REFERENCES workouts(_id), FOREIGN KEY(exercise_id) REFERENCES exercises(_id))");
+    }
+
+    // Methode zum Aktualisieren der Datenbank
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Hier implementieren Sie den Code, um Ihre Datenbank zu aktualisieren, falls die Versionsnummer erhöht wird
+    }
 }
